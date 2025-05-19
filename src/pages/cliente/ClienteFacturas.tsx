34d623b427d699d81c3bc1d89ec40cd960da336d
EspinacaLaser <ignacioramosmartin18@gmail.com>
2025-05-19 20:23:25 +0200

    tsx de clientes basiquisimos

---
+++ src/pages/cliente/ClienteFacturas.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ClienteFacturas: React.FC = () => {
+  return <h1>Cliente - Facturas</h1>;
+};
+
+export default ClienteFacturas;
---
+++ src/pages/cliente/ClienteReservas.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ClienteReservas: React.FC = () => {
+  return <h1>Cliente - Mis Reservas</h1>;
+};
+
+export default ClienteReservas;
